e97b552995ca4342d2ae42681daa98ddd85f92eb
National-student <sanjarshopulatov62@gmail.com>
2022-03-14 10:00:35 +0500

    initail

--- js/index.js
+++ js/index.js
@@ -1,1 +1,37 @@
+var elDisplay = document.querySelector('.d-flex');
+var elCard = document.querySelector('.card');
+var elImg = document.querySelector('.card-img-top');
+var elCardBody = document.querySelector('.film-year');
+var elTitle = document.querySelector('.card-title');
+var elFilmYearDiv = document.querySelector('.film-year-div');
+var elFilmYear = document.querySelector('.film-year');
+var elFilmRatingDiv = document.querySelector('.film-rating-div');
+var elFilmRating = document.querySelector('.film-rating');
 
+for (var result of films) {
+    
+    // CREATE ELEMENT
+
+    var elDiv = document.createElement('div');
+    var elImg = document.createElement('img');
+
+    
+    // CLASSLIST ADD
+
+    elDiv.classList.add('card');
+    elImg.classList.add('card-top-img');
+
+
+
+    
+    //TEXTCONTENT
+    elImg.textContent = result.poster;
+ 
+ 
+    // APPENDCHILD
+
+    elDisplay.appendChild(elDiv)
+    elDiv.appendChild(elImg);
+
+
+}
